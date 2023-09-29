c755893fed40e10a8ad3aee84014c6d29a8a5042
waltindobne <145157901+waltindobne@users.noreply.github.com>
2023-09-29 15:40:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("digite o valor do capital a ser aplicado");
+        double c = double.Parse(Console.ReadLine());
+        Console.WriteLine("digite o a taxa do juros aplicado ");
+        double i = double.Parse(Console.ReadLine());
+        i = i/100;
+        Console.WriteLine("digite a quantidade de meses que deve ser pago o emprestimo");
+        double n = double.Parse(Console.ReadLine());
+        double i2 = 1+i;
+        double resulti = Math.Pow(i2,n);
+        double m = c*resulti;
+        Console.Write("montante final: ");
+        Console.WriteLine($"{m:C}");
+    }
+}
